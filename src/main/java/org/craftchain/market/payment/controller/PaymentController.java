344568a002bca70c240661824999cb00b190428e
Anicet Togue <atogue@gmail.com>
2021-01-12 17:21:18 +0100

    fix dependency injection

--- src/main/java/org/craftchain/market/payment/controller/PaymentController.java
+++ src/main/java/org/craftchain/market/payment/controller/PaymentController.java
@@ -2,6 +2,7 @@ package org.craftchain.market.payment.controller;
 
 import org.craftchain.market.payment.entity.Payment;
 import org.craftchain.market.payment.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/payment")
 public class PaymentController {
 
+    @Autowired
     private PaymentService service;
 
     @PostMapping("/doPayment")
