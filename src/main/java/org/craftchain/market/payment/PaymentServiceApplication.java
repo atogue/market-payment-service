25048d59e490296bdf32ea02df9efbd97de03553
Anicet Togue <atogue@gmail.com>
2021-01-14 15:34:26 +0100

    enable eureka client

--- src/main/java/org/craftchain/market/payment/PaymentServiceApplication.java
+++ src/main/java/org/craftchain/market/payment/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package org.craftchain.market.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
